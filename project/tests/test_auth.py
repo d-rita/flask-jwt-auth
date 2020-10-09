4ff154e4ffa19e4365bb485f3224eb0ee2d71d6e
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-10-09 14:14:32 +0300

    Add tests for login route

--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -5,23 +5,35 @@ from project.server import db
 from project.server.models import User
 from project.tests.base import BaseTestCase
 
-def register_user(self, email, password):
-    return self.client.post(
-        '/auth/register',
-        data=json.dumps(dict(
-            email=email,
-            password=password
-        )),
-        content_type='application/json'
-    )
+
 
 class TestAuthBlueprint(BaseTestCase):
+
+    def register_user(self, email, password):
+        return self.client.post(
+            '/auth/register',
+            data=json.dumps(dict(
+                email=email,
+                password=password
+            )),
+            content_type='application/json'
+        )
+
+    def login_user(self, email, password):
+        return self.client.post(
+            '/auth/login',
+            data=json.dumps(dict(
+                email=email,
+                password=password
+            )),
+            content_type='application/json'
+        )
     
     def test_registration_succeeds(self):
         """
         Test for user registration
         """
-        response = register_user(self, 'user1@test.com', '123456')
+        response = self.register_user('user1@test.com', '123456')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
         self.assertTrue(data['message'] == 'Successfully registered.')
@@ -39,11 +51,38 @@ class TestAuthBlueprint(BaseTestCase):
         )
         db.session.add(user)
         db.session.commit()
-        response = register_user(self, 'user1@test.com', '123456')
+        response = self.register_user('user1@test.com', '123456')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'User already exists! Please log in.')
         self.assertTrue(response.content_type == 'application/json')
         self.assertEqual(response.status_code, 202)
+
+
+    def test_registered_user_login_succeeds(self):
+        """
+        Test successful login of registered user
+        """
+        user_register = self.register_user('user1@test.com', '123456')
+        user_login_response = self.login_user('user1@test.com', '123456')
+        data = json.loads(user_login_response.data.decode())
+        self.assertTrue(data['status'] == 'success')
+        self.assertTrue(data['message'] == 'Successfully logged in.')
+        self.assertTrue(data['auth_token'])
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_unregistered_user_login_fails(self):
+        """
+        Test failed login of unregistered user
+        """
+        user_login_response = self.login_user('user1@test.com', '123456')
+        data = json.loads(user_login_response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(data['message'] == 'Successfully logged in.')
+        self.assertTrue(data['auth_token'])
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 401)
+
 if __name__ == '__main__':
     unittest.main()
